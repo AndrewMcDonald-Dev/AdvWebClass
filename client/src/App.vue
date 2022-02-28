160bc71ce88aaeb161395af6e4dff48142d72d6d
Andrew McDonald <andrewmcd.dev@gmail.com>
2022-02-28 15:06:37 -0500

    Client working just as much as noFramwork/vue.html

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import Nav from './components/Nav.vue';
+import Content from './components/Content.vue';
 </script>
 
 <template>
   <Nav />
-  <h1 class="title">Praying for Ukraine</h1>
+  <div class="container">
+    <section class="section">
+      <h1 class="title">Praying for Ukraine</h1>
+    </section>
+  </div>
+  <Content />
 </template>
 
 <style>
