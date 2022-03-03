725c2b38de9b7a28bdcbf6bb8e0e7144bc52e9d3
Andrew McDonald <andrewmcd.dev@gmail.com>
2022-03-03 15:16:34 -0500

    Added basic route restrictions

---
+++ client/src/pages/Login.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="section">
+        <h1 class="title">Login Page</h1>
+        <button class="button is-primary">
+            <span class="icon">
+                <i class="fa fa-sign-in"></i>
+            </span>
+            <span>Login</span>
+        </button>
+    </div>
+</template>
+
+<script></script>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import session from "../models/session";
 import Generic from "../pages/Generic.vue";
 import Home from "../pages/Home.vue";
-import Messages from "../pages/Messages.vue";
+import Login from "../pages/Login.vue";
+// import Messages from "../pages/Messages.vue";
 
 // 2. Define some routes
 // Each route should map to a component.
@@ -10,9 +12,9 @@ const routes: RouteRecordRaw[] = [
     { path: "/", component: Home },
     { path: "/about", component: Generic, props: { title: "About Page" } },
     { path: "/contact", component: Generic, props: { title: "Contact Page" } },
-    { path: "/login", component: Generic, props: { title: "Login Page" } },
+    { path: "/login", component: Login },
     { path: "/signup", component: Generic, props: { title: "Signup Page" } },
-    { path: "/messages", component: Messages },
+    { path: "/messages", component: () => import("../pages/Messages.vue") },
 ];
 
 // 3. Create the router instance and pass the `routes` option
@@ -23,4 +25,12 @@ export const router = createRouter({
     history: createWebHistory(),
     routes, // short for `routes: routes`
     linkActiveClass: "is-active",
+});
+
+router.beforeEach((to, from) => {
+    if (["/messages"].includes(to.path)) {
+        if (!session.user) {
+            router.push("/login");
+        }
+    }
 });
