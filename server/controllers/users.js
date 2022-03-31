f450d139d914e83757515a940dc9ac7219633fcb
AndrewMcDonald-Dev <AndrewMcDonald.Dev@gmail.com>
2022-03-31 19:15:34 +0000

    Deploying to node-deploy from @ AndrewMcDonald-Dev/AdvWebClass@78ca20e5b26749364008f75b0dad094215f065bc 🚀

---
+++ server/controllers/users.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express.Router();
+const userModel = require("../models/user");
+const StatusCodes = require("http-status-codes");
+
+app.get("/", (req, res) => {
+    res.send(userModel.list);
+})
+    .get("/:id", (req, res) => {
+        const user = userModel.get(req.params.id);
+        res.send(user);
+    })
+    .post("/", (req, res) => {
+        userModel
+            .create(req.body)
+            .then((user) => {
+                res.status(StatusCodes.CREATED).send(user);
+            })
+            .catch(next);
+    })
+    .delete("/:id", (req, res) => {
+        const user = userModel.remove(req.params.id);
+        res.send({ succes: true, erros: [], data: user });
+    })
+    .patch("/:id", (req, res) => {
+        const user = userModel.update(req.params.id, req.body);
+        res.send({ succes: true, erros: [], data: user });
+    });
+
+module.exports = app;
---
+++ server/models/user.js
@@ -0,0 +1,68 @@
+const bcrypt = require("bcrypt");
+
+const list = [
+    {
+        firstName: "Andrew",
+        lastName: "McDonald",
+        email: "simple@email.com",
+        handle: "cool",
+        id: 1,
+        password: "qwerty",
+        pic: "https://randomuser.me/portraits/men/1.jpg",
+    },
+    {
+        firstName: "Koolaid",
+        lastName: "Guy",
+        email: "ohyeag@email.com",
+        handle: "kool",
+        id: 2,
+        password: "qwerty",
+        pic: "someurl",
+    },
+    {
+        firstName: "Type",
+        lastName: "Script",
+        email: "is@email.com",
+        handle: "awesome",
+        id: 3,
+        password: "qwerty",
+        pic: "someurl",
+    },
+];
+let highestId = 3;
+
+const get = (id) => ({
+    ...list.find((user) => user.id === parseInt(id)),
+    password: undefined,
+});
+
+const remove = (id) => {
+    const index = list.findIndex((user) => user.id === parseInt(id));
+    return { ...list.splice(index, 1)[0], password: undefined };
+};
+const update = (id, user) => {
+    const index = list.findIndex((user) => user.id === parseInt(id));
+    const newUser = Object.assign(list[index], user);
+    return { ...newUser, password: undefined };
+};
+
+module.exports = {
+    async create(user) {
+        user.id = ++highestId;
+
+        user.password = await bcrypt.hash(
+            user.password,
+            +process.env.SALT_ROUNDS
+        );
+
+        list.push(user);
+        return { ...user, password: undefined };
+    },
+    remove,
+    update,
+    get list() {
+        return list.map((user) => ({ ...user, password: undefined }));
+    },
+};
+
+module.exports.get = get;
