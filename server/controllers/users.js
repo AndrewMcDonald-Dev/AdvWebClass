1d89619923f6044a82679b6dfe5f9954fff8fc4f
Andrew McDonald <andrewmcd.dev@gmail.com>
2022-04-04 15:16:52 -0400

    Added auth and error handling

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express.Router();
+
+const { requireAuth } = require("../models/auth");
 const userModel = require("../models/user");
 const StatusCodes = require("http-status-codes");
+const { reset } = require("nodemon");
 
-app.get("/", (req, res) => {
+app.get("/", requireAuth, (req, res) => {
     res.send(userModel.list);
 })
     .get("/:id", (req, res) => {
         const user = userModel.get(req.params.id);
         res.send(user);
     })
-    .post("/", (req, res) => {
+    .post("/", requireAuth, (req, res) => {
         userModel
             .create(req.body)
             .then((user) => {
@@ -18,13 +21,21 @@ app.get("/", (req, res) => {
             })
             .catch(next);
     })
-    .delete("/:id", (req, res) => {
+    .delete("/:id", requireAuth, (req, res) => {
         const user = userModel.remove(req.params.id);
         res.send({ succes: true, erros: [], data: user });
     })
     .patch("/:id", (req, res) => {
         const user = userModel.update(req.params.id, req.body);
         res.send({ succes: true, erros: [], data: user });
+    })
+    .post("login", (req, res) => {
+        userModel
+            .login(req.params.email, req.params.password)
+            .then((user) => {
+                res.send(user);
+            })
+            .catch(next);
     });
 
 module.exports = app;
