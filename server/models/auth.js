e0647165a83aed6a15e3fb6a8767ceede6ecadf8
Andrew McDonald <andrewmcd.dev@gmail.com>
2022-04-07 11:28:42 -0400

    Fixed use of http-status-codes

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,4 +1,4 @@
-const StatusCodes = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 module.exports = {
     requireAuth: (req, res, next) => {
         if (!req.user)
