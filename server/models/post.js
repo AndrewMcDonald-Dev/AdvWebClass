71191dc0068cd9598757e8dfb39d5be50281ba71
Andrew McDonald <andrewmcd.dev@gmail.com>
2022-04-11 15:16:40 -0400

    db CRUD for posts just needs /wall

--- server/models/post.js
+++ server/models/post.js
@@ -1,12 +1,15 @@
 const userModel = require("./user");
 const { StatusCodes } = require("http-status-codes");
+const { db, OnjectId } = require("./mongo");
+
+const postCollection = db.db("myFirstDatabase").collection("posts");
 
 const list = [
     {
         src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-560x280,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
         caption:
             "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
-        owner: 1,
+        owner: "cool",
         likes: [],
         comments: [],
         isPublic: true,
@@ -16,7 +19,7 @@ const list = [
         src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-560x280,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
         caption:
             "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
-        owner: 1,
+        owner: "cool",
         likes: [],
         comments: [],
         isPublic: true,
@@ -26,7 +29,7 @@ const list = [
         src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-560x280,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
         caption:
             "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
-        owner: 1,
+        owner: "kool",
         likes: [],
         comments: [],
         isPublic: true,
@@ -36,37 +39,46 @@ const list = [
 
 let highestId = 3;
 
-const includeUser = (post) => ({ ...post, user: userModel.get(post.owner) });
+const includeUser = async (post) => ({
+    ...post,
+    user: await userModel.getByHandle(post.owner),
+});
 
-const get = (id) => {
-    const post = list.find((post) => post.id === parseInt(id));
+const get = async (id) => {
+    // const post = list.find((post) => post.id === parseInt(id));
+    const post = await postCollection.findOne({ _id: new OnjectId(id) });
     if (!post)
         throw { statusCode: StatusCodes.NOT_FOUND, message: "Post not found" };
-    return includeUser(post);
+    return await includeUser(post);
 };
 
-const remove = (id) => {
-    const index = list.findIndex((user) => user.id === parseInt(id));
-    const post = list.splice(index, 1)[0];
-    return includeUser(post);
+const remove = async (id) => {
+    const post = await postCollection.findOneAndDelete({
+        _id: new ObjectId(id),
+    });
+    return await includeUser(post);
 };
-const update = (id, newPost) => {
-    const index = list.findIndex((user) => user.id === parseInt(id));
-    const oldPost = list[index];
-    newPost = list[index] = Object.assign(oldPost, user);
-    return includeUser(newPost);
+const update = async (id, newPost) => {
+    newPost = await postCollection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: newPost },
+        { returnDocument: "after" }
+    );
+    return await includeUser(newPost);
 };
 
 module.exports = {
-    create(post) {
-        post.id = ++highestId;
-        list.push(post);
-        return includeUser(post);
+    async create(post) {
+        const result = await postCollection.insertOne(post);
+        post = await get(result.insertedId);
+        return await includeUser(post);
     },
     remove,
     update,
-    get list() {
-        return list.map((post) => includeUser(post));
+    postCollection,
+    async getList() {
+        const posts = await postCollection.find().toArray();
+        return Promise.all(posts.map((post) => includeUser(post)));
     },
 };
 
